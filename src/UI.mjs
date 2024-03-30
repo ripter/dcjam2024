a85766ced813ec86efe477b20cfed034696e7539
ripter <ripter001@gmail.com>
2024-03-29 23:54:13 -0400

    removing old code

--- src/UI.mjs
+++ src/UI.mjs
@@ -1,16 +1,9 @@
-import { Vector3 } from '/libs/three.module.js';
 import { 
   Application, 
   Assets,
-  Graphics, 
-  Container, 
   Sprite,
-  Text, 
-  TextStyle, 
-  FillGradient,
 } from '../libs/pixi.min.mjs';
 import { Minimap } from './Minimap.mjs';
-import { xyToIndex } from './xyToIndex.mjs';
 
 export class UI {
   #level;
@@ -103,117 +96,5 @@ export class UI {
       miniMapPadding
     );
   }
-
-  /**
-   * Adds the mini map to the UI.
-   * Removes any old mini map that exists. 
-   */
-  async addMiniMap3() {
-    const { 
-      root, 
-      mask, 
-      tileMap, 
-      tileSize, 
-      background,
-      miniMapWidth, miniMapHeight,
-      padding,
-    } = this.miniMap;
-    const level = this.#level;
-
-    console.log('Drawing Background', miniMapWidth, miniMapHeight)
-    // Draw the mini map outline
-    // background.rect(0, 0, this.miniMapWidth, this.miniMapHeight);
-    background.rect(0, 0, 100, 100);
-    background.stroke({ width: this.lineWidth, color: 0xffffff });
-    background.fill({ color: 0xff00ff });
-
-    // root.addChild(background);
-    // this.app.stage.addChild(root);
-    // Position the mini map
-    // root.x = 100;
-    root.x = this.screenWidth - miniMapWidth - padding;
-    console.log('Root X:', root.x);
-    // root.y = this.padding;
-
-    //
-    // Add each tile in the level to the mini map
-    // for (let x = 0; x < level.widthInTiles; x++) {
-    //   for (let y = 0; y < level.heightInTiles; y++) {
-    //     const tile = level.getTileBy3DPosition(new Vector3(x, 0, y));
-    //     const texture = await Assets.load(tile.sprite); // PIXI.JS says this is the correct way to load a texture. They handle cache.
-    //     const sprite = new Sprite(texture);
-    //     const scale = tileSize / sprite.width; // This assumes a square texture for simplicity
-    //     sprite.scale.set(scale, scale);
-    //     sprite.position.set(x * tileSize, y * tileSize);
-    //     console.log('Adding tile to mini map:', tile, sprite);
-    //     tileMap.addChild(sprite);
-    //   }
-    // }
-  }
-  async addMiniMap2() {
-    // Clear any old that exists
-    this.app.stage.removeChild(this.miniMapContainer);
-    this.miniMapContainer = new Container();
-    // Calculate the size and position of the mini map
-    const { app, miniMapContainer } = this;
-    // const { width, height } = app.screen;
-
-    // Fill it with the map tiles
-    await this.addMapTiles();
-
-    // Draw the mini map outline
-    miniMapContainer.rect(0, 0, this.miniMapWidth, this.miniMapHeight);
-    miniMapContainer.stroke({ width: this.lineWidth, color: 0xffffff });
-
-    // Position the mini map
-    miniMapContainer.x = this.screenWidth - this.miniMapWidth - this.padding;
-    miniMapContainer.y = this.padding;
-
-
-    // Add the Player to the miniMapContainer
-    const texture = await Assets.load('assets/player.webp');
-    this.player = new Sprite(texture);
-    this.player.anchor.set(0.5, 0.5);
-    const scale = this.tileSize / this.player.width;
-    this.player.scale.set(scale, scale);
-    miniMapContainer.addChild(this.player);
-
-    app.stage.addChild(miniMapContainer);
-    return miniMapContainer;
-  }
-
-  /**
-   * Adds the map tiles to the mini map. 
-   */
-  async addMapTiles2() {
-    const { tileSize, root } = this.miniMap;
-    const level = this.#level;
-    // const root = new Container();
-    // this.miniMap = root;
-
-    // Add each tile in the level to the mini map
-    for (let x = 0; x < level.widthInTiles; x++) {
-      for (let y = 0; y < level.heightInTiles; y++) {
-        const tile = level.getTileBy3DPosition(new Vector3(x, 0, y));
-        const texture = await Assets.load(tile.sprite); // PIXI.JS says this is the correct way to load a texture. They handle cache.
-        const sprite = new Sprite(texture);
-        const scale = tileSize / sprite.width; // This assumes a square texture for simplicity
-        sprite.scale.set(scale, scale);
-        sprite.position.set(x * tileSize, y * tileSize);
-        root.addChild(sprite);
-      }
-    }
-
-    // Create a mask for the mini map
-    const mask = new Graphics();
-    mask.rect(0, 0, this.miniMapWidth, this.miniMapHeight);
-    mask.fill({color: 'black'});
-    // root.mask = mask;
-    // Add the mask and the root to the mini map
-    this.miniMapContainer.addChild(mask);
-    this.miniMapContainer.addChild(root);
-    return root;
-  }
-
 
 }
