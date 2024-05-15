9bb32e5f378972e5e9ebad6f7b6b6a9a8d0762e9
ripter <ripter001@gmail.com>
2024-05-14 21:59:57 -0400

    minimap refactor uses new iterateFloor

--- src/UI/Minimap.mjs
+++ src/UI/Minimap.mjs
@@ -1,4 +1,6 @@
 import { Container, Graphics } from '../../libs/pixi.min.mjs';
+import { DEFAULT_TILE_SIZE } from '../consts.mjs';
+import { loadSprite } from './loadSprite.mjs';
 
 export class Minimap {
   constructor() {
@@ -6,12 +8,19 @@ export class Minimap {
   }
 
   async init() {
+    this.scene.position.set(0, 0);
   }
 
   async loadLevel(level) {
     // Draw the floor map
-    level.floorMap.forEach((tileId, index) => {
-      console.log('tileId:', tileId, 'index:', index);
-    });
+    for (const floorTile of level.iterateFloor()) {
+      const { spriteUrl } = floorTile;
+      if (!spriteUrl) {
+        continue;
+      }
+      const sprite = await loadSprite(spriteUrl, DEFAULT_TILE_SIZE);
+      sprite.position.set(floorTile.position.x * DEFAULT_TILE_SIZE, floorTile.position.y * DEFAULT_TILE_SIZE);
+      this.scene.addChild(sprite);
+    }
   }
 }
